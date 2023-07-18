22831acb89512474cdb81e670fa3d6016a4b9aa7
dihjital <peter.hrobar@gmail.com>
2023-07-18 16:32:06 +0200

    Bug fixes and performance improvements

--- app/Jobs/GatherIpLocation.php
+++ app/Jobs/GatherIpLocation.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use App\Events\VoteAttachedToLocation;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 
 use App\Models\Location;
 
@@ -36,35 +37,55 @@ class GatherIpLocation extends Job
         // TODO: Wait 3 seconds so I can test QUEUE_CONNECTION ... should be removed from PROD
         sleep(3);
 
-        $response = Http::get($this->url, [
-            'ip_address' => $this->ipAddress,
-        ]);
-
-        // Handle the responses
-        // TODO: Check returned data ...
-        if (!$response->successful()) {
-            Log::error($response->status().":".$response->body());
-               throw new \Exception($response->body());
-        }
-
-        $data = $response->json()['data'];
-
-        $location = new Location;
-        try {
-            $location->fill([
-                'ip' => $data['ip'],
-                'country_name' => $data['country_name'],
-                'city' => $data['city'],
-                'latitude' => $data['latitude'],
-                'longitude' => $data['longitude'],
-            ]);
-            $location->save();
-
+        if ($location = self::isLocationExists($this->ipAddress)) {
+            Log::info(__('Location already exists: ').$this->ipAddress);
             event(new VoteAttachedToLocation($location, $this->voteId));
-            // $location->votes()->attach($this->vote_id);
-        } catch (\Exception $e) {
-            Log::error(__('Failed to save location: ').$e->getMessage());
+        } else {
+            $response = Http::get($this->url, [
+                'ip_address' => $this->ipAddress,
+            ]);
+
+            // Handle the responses
+            // TODO: Check returned data ...
+            if (!$response->successful()) {
+                Log::error($response->status().":".$response->body());
+                throw new \Exception($response->body());
+            }
+
+            $data = $response->json()['data'];
+
+            $location = new Location;
+            try {
+                $location->fill([
+                    'ip' => $data['ip'],
+                    'country_name' => $data['country_name'],
+                    'city' => $data['city'],
+                    'latitude' => $data['latitude'],
+                    'longitude' => $data['longitude'],
+                ]);
+                $location->save();
+
+                event(new VoteAttachedToLocation($location, $this->voteId));
+                // $location->votes()->attach($this->vote_id);
+            } catch (\Exception $e) {
+                Log::error(__('Failed to save location: ').$e->getMessage());
+            }
         }
 
+    }
+
+    public function middleware()
+    {
+        return [(new WithoutOverlapping($this->ipAddress))->releaseAfter(15)->expireAfter(180)];
+    }
+
+    protected static function isLocationExists($ipAddress)
+    {
+        try {
+            $location = Location::where('ip', $ipAddress)->firstOrFail();
+        } catch (\Exception $e) {
+            return false;
+        }
+        return $location;
     }
 }
