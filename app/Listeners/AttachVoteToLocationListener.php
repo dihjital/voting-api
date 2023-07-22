b2e29e9e23c796576fe4ccc5d633bfa21f2e94dc
dihjital <peter.hrobar@gmail.com>
2023-07-22 12:13:47 +0200

    Bug fixes and performance improvements

--- app/Listeners/AttachVoteToLocationListener.php
+++ app/Listeners/AttachVoteToLocationListener.php
@@ -5,7 +5,7 @@ namespace App\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Events\VoteAttachedToLocation;
 
-class AttachVoteToLocationListener implements ShouldQueue
+class AttachVoteToLocationListener
 {
     /**
      * Handle the event.
