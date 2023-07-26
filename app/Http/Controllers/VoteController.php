8cbf06b2054773b1ad1af2990cf6542e4fd3c45a
dihjital <peter.hrobar@gmail.com>
2023-07-26 12:19:09 +0200

    Bug fixes and performance improvements

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -346,9 +346,8 @@ class VoteController extends Controller
             ->sendPushNotification();
 
           // This is where we also gather the voter location based on the request IP address
-          // $this->initWithIpLocation($new_vote->id)->gatherIpLocation(request()->ip());
           $this->initWithIpLocation($new_vote->id)
-               ->gatherIpLocationIf(self::isValidIpAddress(...), '176.77.152.134');
+               ->gatherIpLocationIf(self::isValidIpAddress(...), request()->ip());
         } catch (\Exception $e) {
           return response()->json(self::eWrap($e->getMessage()), 500);
         }
