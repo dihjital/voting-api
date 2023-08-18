fc2d4505bcb0d51f0bf33d115ac5e1acf9edbe0f
dihjital <peter.hrobar@gmail.com>
2023-08-18 09:52:35 +0200

    Bug fixes and performance improvements

--- app/Actions/IncreaseVoteNumber.php
+++ app/Actions/IncreaseVoteNumber.php
@@ -3,8 +3,6 @@
 namespace App\Actions;
 
 use App\Models\Vote;
-use App\Models\Question;
-use Illuminate\Support\Facades\Validator;
 
 class IncreaseVoteNumber extends VoteActions
 {
@@ -15,19 +13,7 @@ class IncreaseVoteNumber extends VoteActions
      */
     public function increase(array $input): Vote
     {
-        $validator = Validator::make($input, [
-            'user_id' => 'required|uuid',
-        ]);
-      
-        if ($validator->fails()) {
-            throw new \Exception($validator->errors()->first(), 400);
-        }
-      
-        try {
-            $question = Question::whereId($input['question_id'])->where('user_id', $input['user_id'])->firstOrFail();
-        } catch (\Exception $e) {
-            throw new \Exception(__('Question not found'), 404);
-        }
+        $question = $this->findQuestionForVote($input);
         
         $newVote = $question->votes->where('id', '=', $input['vote_id'])->first();
       
--- app/Actions/ShowOneQuestion.php
+++ app/Actions/ShowOneQuestion.php
@@ -2,9 +2,7 @@
 
 namespace App\Actions;
 
-use App\Models\Vote;
 use App\Models\Question;
-use Illuminate\Support\Facades\Validator;
 
 class ShowOneQuestion extends QuestionActions
 {
@@ -15,19 +13,8 @@ class ShowOneQuestion extends QuestionActions
      */
     public function show(array $input): Question
     {
-        // user_id is optional till full transition
-        $validator = Validator::make($input, [
-            'user_id' => 'nullable|uuid',
-        ]);
-      
-        if ($validator->fails()) {
-            throw new \Exception($validator->errors()->first(), 400);
-        }
-      
         try {
-            $question = isset($input['user_id']) 
-              ? Question::whereId($input['question_id'])->where('user_id', $input['user_id'])->firstOrFail()
-              : Question::findOrFail($input['question_id']);
+            $question = $this->findQuestionForUserId($input);
         } catch (\Exception $e) {
             throw new \Exception(__('Question not found'), 404);
         }
