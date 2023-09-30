363caad1464c033941355d64e5036900595c2bfd
dihjital <peter.hrobar@gmail.com>
2023-09-30 18:35:57 +0200

    Bug fixes and performance improvements

--- app/Actions/DeleteToken.php
+++ app/Actions/DeleteToken.php
@@ -15,22 +15,19 @@ class DeleteToken extends TokenActions
     public function delete(array $input): bool
     {
         $this->validateUser($input);
-
         $key = $input['user'];
+        
+        $subscribers = Cache::tags('fcm')->get('subscribers', []);
 
-        if (Cache::tags('fcm')->has('subscribers')) {
-            $subscribers = Cache::tags('fcm')->get('subscribers');
-
-            if (array_key_exists($key, $subscribers)) {
-                foreach ($subscribers[$key] as $token) {
-                    Log::info('Token: '.$token.' deleted successfully for: '.$key);
-                }
-
-                unset($subscribers[$key]);
-                self::refreshCache($subscribers);
-                
-                return true;
+        if (isset($subscribers[$key])) {
+            foreach ($subscribers[$key] as $token) {
+                Log::info('Token: ' . $token . ' deleted successfully for: ' . $key);
             }
+
+            unset($subscribers[$key]);
+            self::refreshCache($subscribers);
+            
+            return true;
         }
 
         return false;
