2b4c04633fdc7bfc8015f5308160861f2198d592
dihjital <peter.hrobar@gmail.com>
2024-03-09 09:50:45 +0100

    Bug fixing and performance improvements

--- app/Console/Commands/CloseQuestions.php
+++ app/Console/Commands/CloseQuestions.php
@@ -40,6 +40,7 @@ class CloseQuestions extends Command
                         Log::info('app:close-questions is closing question: ' . $question->question_text);
                     });
                 })
+                ->get()
                 ->each
                 ->update(['is_closed' => 1]);
 
