ce8c1deee99e9a846beefbad7d951aac0f52b2e0
dihjital <peter.hrobar@gmail.com>
2024-10-15 11:57:36 +0200

    quickchart php plugin added

--- app/Jobs/EmailResultsToVoter.php
+++ app/Jobs/EmailResultsToVoter.php
@@ -7,6 +7,8 @@ use App\Models\Question;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
+use QuickChart;
+
 class EmailResultsToVoter extends Job
 {
     private $templateId;
@@ -74,37 +76,271 @@ class EmailResultsToVoter extends Job
 
     protected function createChartUrl()
     {
-        $chartURL =  
-            'https://quickchart.io/chart/?c=' .
-            http_build_query([
-                'type' => 'bar',
-                'data' => [
-                    'labels' => 
-                        implode(',', 
-                            $this->question->votes->map(
-                                fn($vote, $index) => $this->letters[$index] . ') '
-                            )->toArray()
-                        ),
-                    'datasets' => [
-                        'data' => 
-                            implode(',', 
-                                $this->question->votes->map(
-                                    function ($vote) {
-                                        return $vote->number_of_votes;
-                                    },
-                                )->toArray(),
-                            ),
+        $qc = new QuickChart([
+            'width' => 300,
+            'height' => 200,
+        ]);
+
+        $config = <<<EOD
+
+        {
+            "type": "bar",
+            "data": {
+                "datasets": [
+                {
+                    "label": "Dataset 1",
+                    "data": [
+                    5,
+                    10,
+                    15,
+                    20
                     ],
-                ],
-                'options' => [
-                    'plugins' => [
-                        'roundedBars' => true,
+                    "backgroundColor": [
+                    "lightblue",
+                    "lightblue",
+                    "red",
+                    "lightblue"
                     ],
+                    "fill": false,
+                    "spanGaps": false,
+                    "lineTension": 0.4,
+                    "pointRadius": 3,
+                    "pointHoverRadius": 3,
+                    "pointStyle": "circle",
+                    "borderDash": [
+                    0,
+                    0
+                    ],
+                    "barPercentage": 0.9,
+                    "categoryPercentage": 0.8,
+                    "type": "bar",
+                    "hidden": false
+                }
                 ],
-            ]);
-        
-        Log::debug('chartURL: ' . $chartURL);
-        return $chartURL;
+                "labels": [
+                "Label 1",
+                "Label 2",
+                "Label 3",
+                "Label 4"
+                ]
+            },
+            "options": {
+                "title": {
+                "display": false,
+                "position": "top",
+                "fontSize": 12,
+                "fontFamily": "sans-serif",
+                "fontColor": "#666666",
+                "fontStyle": "bold",
+                "padding": 10,
+                "lineHeight": 1.2,
+                "text": "Chart title"
+                },
+                "layout": {
+                "padding": {},
+                "left": 0,
+                "right": 0,
+                "top": 0,
+                "bottom": 0
+                },
+                "legend": {
+                "display": false,
+                "position": "top",
+                "align": "center",
+                "fullWidth": true,
+                "reverse": false,
+                "labels": {
+                    "fontSize": 12,
+                    "fontFamily": "sans-serif",
+                    "fontColor": "#666666",
+                    "fontStyle": "normal",
+                    "padding": 10
+                }
+                },
+                "scales": {
+                "xAxes": [
+                    {
+                    "display": true,
+                    "id": "X1",
+                    "position": "bottom",
+                    "type": "category",
+                    "stacked": false,
+                    "offset": true,
+                    "time": {
+                        "unit": false,
+                        "stepSize": 1,
+                        "displayFormats": {
+                        "millisecond": "h:mm:ss.SSS a",
+                        "second": "h:mm:ss a",
+                        "minute": "h:mm a",
+                        "hour": "hA",
+                        "day": "MMM D",
+                        "week": "ll",
+                        "month": "MMM YYYY",
+                        "quarter": "[Q]Q - YYYY",
+                        "year": "YYYY"
+                        }
+                    },
+                    "distribution": "linear",
+                    "gridLines": {
+                        "display": true,
+                        "color": "rgba(0, 0, 0, 0.1)",
+                        "borderDash": [
+                        1,
+                        1
+                        ],
+                        "lineWidth": 3,
+                        "drawBorder": true,
+                        "drawOnChartArea": false,
+                        "drawTicks": false,
+                        "tickMarkLength": 10,
+                        "zeroLineWidth": 1,
+                        "zeroLineColor": "rgba(0, 0, 0, 0.25)",
+                        "zeroLineBorderDash": [
+                        0,
+                        0
+                        ]
+                    },
+                    "angleLines": {
+                        "display": true,
+                        "color": "rgba(0, 0, 0, 0.1)",
+                        "borderDash": [
+                        0,
+                        0
+                        ],
+                        "lineWidth": 1
+                    },
+                    "pointLabels": {
+                        "display": true,
+                        "fontColor": "#666",
+                        "fontSize": 10,
+                        "fontStyle": "normal"
+                    },
+                    "ticks": {
+                        "display": true,
+                        "fontSize": 12,
+                        "fontFamily": "sans-serif",
+                        "fontColor": "#666666",
+                        "fontStyle": "normal",
+                        "padding": 0,
+                        "stepSize": null,
+                        "minRotation": 0,
+                        "maxRotation": 50,
+                        "mirror": false,
+                        "reverse": false
+                    },
+                    "scaleLabel": {
+                        "display": false,
+                        "labelString": "Axis label",
+                        "lineHeight": 1.2,
+                        "fontColor": "#666666",
+                        "fontFamily": "sans-serif",
+                        "fontSize": 12,
+                        "fontStyle": "normal",
+                        "padding": 4
+                    }
+                    }
+                ],
+                "yAxes": [
+                    {
+                    "type": "logarithmic",
+                    "display": true,
+                    "id": "Y1",
+                    "position": "left",
+                    "stacked": false,
+                    "offset": true,
+                    "time": {
+                        "unit": false,
+                        "stepSize": 1,
+                        "displayFormats": {
+                        "millisecond": "h:mm:ss.SSS a",
+                        "second": "h:mm:ss a",
+                        "minute": "h:mm a",
+                        "hour": "hA",
+                        "day": "MMM D",
+                        "week": "ll",
+                        "month": "MMM YYYY",
+                        "quarter": "[Q]Q - YYYY",
+                        "year": "YYYY"
+                        }
+                    },
+                    "distribution": "linear",
+                    "gridLines": {
+                        "display": false,
+                        "color": "rgba(0, 0, 0, 0.1)",
+                        "borderDash": [
+                        0,
+                        0
+                        ],
+                        "lineWidth": 1,
+                        "drawBorder": false,
+                        "drawOnChartArea": false,
+                        "drawTicks": false,
+                        "tickMarkLength": 10,
+                        "zeroLineWidth": 1,
+                        "zeroLineColor": "rgba(0, 0, 0, 0.25)",
+                        "zeroLineBorderDash": [
+                        0,
+                        0
+                        ]
+                    },
+                    "angleLines": {
+                        "display": true,
+                        "color": "rgba(0, 0, 0, 0.1)",
+                        "borderDash": [
+                        0,
+                        0
+                        ],
+                        "lineWidth": 1
+                    },
+                    "pointLabels": {
+                        "display": true,
+                        "fontColor": "#666",
+                        "fontSize": 10,
+                        "fontStyle": "normal"
+                    },
+                    "ticks": {
+                        "display": false,
+                        "fontSize": 12,
+                        "fontFamily": "sans-serif",
+                        "fontColor": "#666666",
+                        "fontStyle": "normal",
+                        "padding": 0,
+                        "stepSize": null,
+                        "minRotation": 0,
+                        "maxRotation": 50,
+                        "mirror": false,
+                        "reverse": false
+                    },
+                    "scaleLabel": {
+                        "display": false,
+                        "labelString": "Axis label",
+                        "lineHeight": 1.2,
+                        "fontColor": "#666666",
+                        "fontFamily": "sans-serif",
+                        "fontSize": 12,
+                        "fontStyle": "normal",
+                        "padding": 4
+                    }
+                    }
+                ]
+                },
+                "plugins": {
+                "roundedBars": "true"
+                },
+                "grid": {
+                "display": false
+                },
+                "cutoutPercentage": 50,
+                "rotation": -1.5707963267948966,
+                "circumference": 6.283185307179586,
+                "startAngle": -1.5707963267948966
+            }
+        }
+        EOD;
+
+        $qc->setConfig($config);
+        return $qc->getUrl();
     }
 
     protected function createParams()
