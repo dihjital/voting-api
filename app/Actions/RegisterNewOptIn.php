0ef6e26339155e7d6b858db1bd155bc9af631b2e
dihjital <peter.hrobar@gmail.com>
2025-02-08 08:35:01 +0100

    e-mail uniquiness checked with quiz and question id

--- app/Actions/RegisterNewOptIn.php
+++ app/Actions/RegisterNewOptIn.php
@@ -7,6 +7,8 @@ use App\Models\OptInVoter;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Validator;
 
+use Exception;
+
 class RegisterNewOptIn extends VoterActions
 {
     /**
@@ -36,16 +38,32 @@ class RegisterNewOptIn extends VoterActions
         ], [
             'question_id.prohibits' => __('Quiz id should not be present if Question id is set.'),
             'quiz_id.prohibits' => __('Question id should not be present if Quiz id is set.')
-        ]);
+        ])->after(function ($validator) use ($input) {
+            if (! isset($input['email'])) {
+                return;
+            }
+        
+            $q = OptInVoter::where('email', $input['email']);
+        
+            if (! empty($input['question_id'])) {
+                $q->where('question_id', $input['question_id']);
+            } elseif (! empty($input['quiz_id'])) {
+                $q->where('quiz_id', $input['quiz_id']);
+            }
+        
+            if ($q->exists()) {
+                $validator->errors()->add('email', 'This e-mail address is already registered.');
+            }
+        });
       
         if ($validator->fails()) {
-            throw new \Exception($validator->errors()->first(), 400);
+            throw new Exception($validator->errors()->first(), 400);
         }
       
         try {
             OptInVoter::create($input);
-        } catch (\Exception $e) {
-            throw new \Exception($e->getMessage(), 500);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage(), 500);
         }
     }
 }
